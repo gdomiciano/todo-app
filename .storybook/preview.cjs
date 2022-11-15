875a2a562fdeb259572e1c2efbc1c53c18656fb2
Geisy <geisydomiciano@gmail.com>
2022-11-15 22:56:36 +0100

    :wheelchair: basic accessibility testing

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -6,4 +6,32 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  backgrounds: {
+    disable: true,
+    default: '#fff'
+  },
+  layout: 'centered',
 }
+
+export const globalTypes = {
+  theme: {
+      name: 'Theme',
+      description: 'Global theme',
+      defaultValue: 'light',
+      toolbar: {
+          icon: 'lightning',
+          items: ['dark', 'light'],
+          showName: true,
+      },
+  },
+};
+
+
+export const decorators = [(story, context) => {
+  context.globals.background = context.parameters.backgrounds.values.find(val => val.name === context.globals.theme).value
+  context.canvasElement.setAttribute('data-theme', context.globals.theme);
+
+  return {
+  components: { story },
+  template: `<div data-theme="${context.globals.theme}" style="padding: 20rem;"><story /></div>`
+}}];
